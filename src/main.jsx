268e5f1c5311d6c50feb546a5fa05d600dd503d9
1mDolphin <bulletin03@gmail.com>
2024-12-11 20:12:00 +0200

    fix????

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import App from './App.jsx'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/denispetrov'>
       <App />
     </BrowserRouter>
   </React.StrictMode>
