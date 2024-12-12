a55a1f7824471a47c0e90a0b6bc346150f5f664a
1mDolphin <bulletin03@gmail.com>
2024-12-12 02:45:18 +0200

    added bomberman

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -74,17 +74,19 @@ function HomePage() {
                     <div className='about-me'>
                         <h1>My Projects</h1>
                         <div className='cross-line'></div>
-                        <p>During my educational life, I have done some projects to deeply understand how this or thattechnology works. Here are few projects</p>
+                        <p>During my educational life, I have done some projects to deeply understand how this or that technology works. Here are few projects</p>
                     </div>
                     <div className='projects'>
-                        <div className='project-image'></div>
                         <div className='project-info'>
                             <a className='project-name' href='https://github.com/Zewasik/Gachi.js'>Gachi.js</a>
                             <div className='project-descr'>Gachi.js is a lightweight and simple JSX-based framework that allows you to create and manipulate DOM elements. In this documentation, we will explore the features of Gachi.js and provide code examples with explanations to demonstrate how to perform various tasks, such as creating elements, handling events, nesting elements, and adding attributes.</div>
                         </div>
+                        <div className='project-info-bomberman'>
+                            <a className='project-name' href='https://github.com/1mDolphin/Bomberman'>Bomberman-dom</a>
+                            <div className='project-descr'>Bomberman DOM is an electrifying multiplayer party game crafted entirely on a custom-built web framework, bringing the classic explosive action to a new level of accessibility and excitement. Designed to support up to 4 players simultaneously, this game transforms the traditional Bomberman experience into a seamless, browser-based multiplayer showdown.</div>
+                        </div>
                     </div>
                 </div>
-
             </div>
         </>
     )
